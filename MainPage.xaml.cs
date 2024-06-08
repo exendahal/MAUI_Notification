4580e6d8519bb4ccfa3749129e0f4e35b0c986aa
Santosh Dahal <exendahal@gmail.com>
2024-06-08 12:19:57 +0545

    feat: add support to .net 8

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,30 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+    protected override async void OnAppearing()
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.PostNotifications>();
+		if (status != PermissionStatus.Granted)
+		{
+			status = await Permissions.RequestAsync<Permissions.PostNotifications>();
+            if (status != PermissionStatus.Granted)
+			{
+                await DisplayAlert("Permission Denied", "To stay informed about important updates, please enable notification permissions in your settings.", "OK");
+            }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        }
+        base.OnAppearing();
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.PostNotifications>();
+        var result = status == PermissionStatus.Granted ? "Granted" : "Not granted";
+        await DisplayAlert("MAUI Notification", $"Notification permission: {result}","OK");
+    }
 }
 
